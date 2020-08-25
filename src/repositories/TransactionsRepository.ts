4173c94846186028be73affc2fa82e01f2d227f0
eduqg <eduardoqgomes@gmail.com>
2020-08-25 17:44:28 -0300

    Source to fundamentos-node challenge 5

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,47 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(function (acumulador, transaction) {
+      if (transaction.type === 'income') {
+        return acumulador + transaction.value;
+      }
+      return acumulador;
+    }, 0);
+
+    const outcome = this.transactions.reduce(function (
+      acumulador,
+      transaction,
+    ) {
+      if (transaction.type === 'outcome') {
+        return acumulador + transaction.value;
+      }
+      return acumulador;
+    },
+    0);
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    if (type === 'outcome') {
+      const balance = this.getBalance();
+      if (balance.total < value) {
+        throw Error('Not enought cash to this transaction');
+      }
+    }
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
